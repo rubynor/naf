2848179080cf29886118093dab423b36efb48234
Chrisitan Hager <christian@hager.as>
2011-10-04 16:47:54 +0200

    tweek locations index to match ext.js requirements some more

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,7 @@ class LocationsController < ApplicationController
   respond_to :json
 
   def index
-    render :json => {:success => true, :locations => Location.all}
+    render :json => {:locations => Location.all}
   end
 
 end
