43c51f2b870a481b573dad309a999e23e905a4a8
Chrisitan Hager <christian@hager.as>
2011-10-05 09:16:20 +0200

    adding callback to json response

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,7 @@ class LocationsController < ApplicationController
   respond_to :json
 
   def index
-    respond_with :success => "true", :locations => Location.all
+    respond_with :success => "true", :locations => Location.all, :callback => params[:callback]
   end
 
 end
