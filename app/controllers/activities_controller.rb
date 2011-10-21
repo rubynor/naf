351fec60d8b3e497b8e3d79b5434be749b5b2935
Chrisitan Hager <christian@hager.as>
2011-10-21 11:45:09 +0200

    added resque and error msg in post put of activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -28,22 +28,28 @@ class ActivitiesController < ApplicationController
   end
 
   def create
-    Rails.logger.warn params
-    ap params
+    begin
     @activity = Activity.new(params[:activity])
     if @activity.save
       render :json => @activity, :callback => params[:callback], :status => :ok
     else
       render :json => @activity.errors, :callback => params[:callback], status => :unprocessable_entity
     end
+    rescue => e
+      render :json => {:error => e.messsage, :backtrace => e.backtrace}.to_json
+    end
   end
 
   def update
+    begin
     @activity = Activity.find(params[:id])
     if @activity.update_attributes(params[:activity])
       render :json => @activity, :callback => params[:callback], :status => :ok
     else
       render :json => @activity.errors, :status => :unprocessable_entity
+    end
+    rescue => e
+      render :json => {:error => e.messsage, :backtrace => e.backtrace}.to_json
     end
   end
 
